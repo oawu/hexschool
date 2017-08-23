762410b6d1820836b932a6d3e72193b404dfc062
OA Wu <comdan66@gmail.com>
2017-08-23 19:03:49 +0800

    Copy frame from OAF2E version 4.1.2

---
+++ js/public.js
@@ -0,0 +1,9 @@
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2017 OA Wu Design
+ * @license     http://creativecommons.org/licenses/by-nc/2.0/tw/
+ */
+
+$(function () {
+
+});
